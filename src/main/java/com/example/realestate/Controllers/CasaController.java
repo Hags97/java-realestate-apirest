b4f560872c842377664e0b19ba3999d4cc846a79
Hags97 <hugoags97@gmail.com>
2024-07-21 13:55:29 +0200

    implemtnet metodo update

--- src/main/java/com/example/realestate/Controllers/CasaController.java
+++ src/main/java/com/example/realestate/Controllers/CasaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,5 +59,20 @@ public class CasaController
         {
             throw new RuntimeException("Property not found with id " + id);
         }
+    }
+
+    @PutMapping("/{id}")
+    public Casa updateCasa(@PathVariable Long id, @RequestBody Casa casa)
+    {
+        Casa casaToUpdate = casaRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("No se encontro el producto con el ID: " + id));
+
+        casaToUpdate.setPais(casa.getPais());
+        casaToUpdate.setCiudad(casa.getCiudad());
+        casaToUpdate.setPrecio(casa.getPrecio());
+        casaToUpdate.setDescription(casa.getDescription());
+        casaToUpdate.setUrlImagen(casa.getUrlImagen());
+
+        return casaRepository.save(casaToUpdate);
     }
 }
