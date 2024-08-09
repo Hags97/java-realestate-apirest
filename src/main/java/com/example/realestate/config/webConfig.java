3ca6de291f3b3175aa88280b1cb56aa29d68bb1b
Hugo Garay - Front-End Developer <111492166+Hags97@users.noreply.github.com>
2024-08-09 15:19:49 +0200

    Update webConfig.java

--- src/main/java/com/example/realestate/config/webConfig.java
+++ src/main/java/com/example/realestate/config/webConfig.java
@@ -23,7 +23,7 @@ public class webConfig
            public void addCorsMappings(CorsRegistry registry) 
            {
                registry.addMapping("/**")
-               .allowedOrigins("http://localhost:4200/")
+               .allowedOrigins("*****")
                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name())
                .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
            }
